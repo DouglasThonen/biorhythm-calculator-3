fcc07231c3a39f7738129f4ad48353bcc0768c42
Douglas Thonen <dthonen@gmail.com>
2023-11-12 09:05:48 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { IonApp, IonContent, IonHeader, IonInput, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
+
+function App() {
+  const [name, setName] = useState('');
+  return (
+    <IonApp>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Biorhythms</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        <IonInput placeholder="Your name"
+        value={name} onIonChange={(event) => setName(event.detail.value)}
+        />
+        <p>
+          You entered: <b>{name}</b>
+        </p>
+      </IonContent>
+    </IonApp>
+  );
+}
+
+export default App;
